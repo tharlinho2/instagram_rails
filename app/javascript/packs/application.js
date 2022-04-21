fc5484485e269e139192770b7ed42997acf1b83e
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-04-21 00:41:47 -0300

    add comments

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,6 @@ import Rails from "@rails/ujs";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 
-
-// import "./flashMessages";
-// import "./modal";
-// import "./likes";
-
 require("trix");
 require("@rails/actiontext");
 
