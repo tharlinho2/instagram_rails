cde60d04effb88627016e55c6b9ae28280477bf5
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2022-04-20 23:33:46 -0300

    add like and dislike

--- app/javascript/packs/likes.js
+++ app/javascript/packs/likes.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.actions .like').forEach((button) => {
     button.addEventListener('click', like)
   })
+
+  document.querySelectorAll('.actions .dislike').forEach((button) => {
+    button.addEventListener('click', dislike)
+  })
 })
 
 function like(event) {
@@ -20,10 +24,24 @@ function like(event) {
     .catch((error) => console.log('parsing failed', error))
 }
 
+function dislike(event) {
+  const actionElement = event.target.closest('.action')
+
+  fetch(`/likes/${actionElement.dataset.likeId}`, {
+    method: 'DELETE',
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((json) => handleLikeFeatureCallback({ ...json, actionElement }))
+    .catch((error) => console.log('parsing failed', error))
+}
+
 function handleLikeFeatureCallback({ id, successful, actionElement }) {
   if (successful) {
-    actionElement.querySelectorAll(".like, .dislike").forEach(div => {
-      div.classList.toggle("hidden");
-    });
+    actionElement.dataset.likeId = id;
+    
+    actionElement.querySelectorAll('.like, .dislike').forEach((div) => {
+      div.classList.toggle('hidden')
+    })
   }
 }
